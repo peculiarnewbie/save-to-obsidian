456ecdbc003d64911e4e813208b36c4b9094d4d6
peculiarnewbie <curiousarmand@gmail.com>
2023-07-26 17:39:31 +0700

    prettier resetup

--- src/components/Field.svelte
+++ src/components/Field.svelte
@@ -1,117 +1,117 @@
 <script lang="ts">
-    import select from "../../public/Select.svg";
-    import trash from "../../public/Delete.svg";
-    import { createEventDispatcher } from "svelte";
-    export let index = 0;
-    export let field;
-    $: key = field.key;
-    $: value = field.value;
-    $: treePath = field.treePath;
+	import select from "../../public/Select.svg";
+	import trash from "../../public/Delete.svg";
+	import { createEventDispatcher } from "svelte";
+	export let index = 0;
+	export let field;
+	$: key = field.key;
+	$: value = field.value;
+	$: treePath = field.treePath;
 
-    export let parentInspect;
-    export let isEditing = false;
+	export let parentInspect;
+	export let isEditing = false;
 
-    enum IdType {
-        ID,
-        CLASS,
-        INDEX,
-    }
+	enum IdType {
+		ID,
+		CLASS,
+		INDEX,
+	}
 
-    const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-    // const initField = async() => {
-    //     console.log("init field")
-    //     if(field.treePath){
-    //         await getValueFromPath(field.treePath)
-    //     }
-    // }
+	// const initField = async() => {
+	//     console.log("init field")
+	//     if(field.treePath){
+	//         await getValueFromPath(field.treePath)
+	//     }
+	// }
 
-    const selectElement = () => {
-        document.getElementById("extension-html").classList.add("hidden");
-        parentInspect(index);
-    };
+	const selectElement = () => {
+		document.getElementById("extension-html").classList.add("hidden");
+		parentInspect(index);
+	};
 
-    const getValueFromPath = async (path, selectedElement?) => {
-        let fetching = true;
+	const getValueFromPath = async (path, selectedElement?) => {
+		let fetching = true;
 
-        chrome.runtime.sendMessage(
-            { action: "getElement", path: path },
-            (response) => {
-                if (!response.success) {
-                    console.error("failed to get element");
-                } else {
-                    field.value = response.value;
-                    fetching = false;
-                }
-            },
-        );
+		chrome.runtime.sendMessage(
+			{ action: "getElement", path: path },
+			(response) => {
+				if (!response.success) {
+					console.error("failed to get element");
+				} else {
+					field.value = response.value;
+					fetching = false;
+				}
+			}
+		);
 
-        while (fetching) {
-            await new Promise((r) => setTimeout(r, 10));
-        }
-        console.log("got value", field.value);
-    };
+		while (fetching) {
+			await new Promise((r) => setTimeout(r, 10));
+		}
+		console.log("got value", field.value);
+	};
 
-    const deleteField = () => {
-        dispatch("deleteField", index);
-    };
+	const deleteField = () => {
+		dispatch("deleteField", index);
+	};
 
-    // initField()
+	// initField()
 </script>
 
 <div id="FieldRoot" class="pt-3">
-    <!-- <p>{index}</p> -->
-    {#if isEditing}
-        <input
-            class="font-bold text-base text-white w-full min-w-[40px] h-8 bg-transparent outline-none border-b border-[#3e4446]"
-            type="text"
-            placeholder="enter key"
-            bind:value={field.key}
-        />
-        <div id="FieldComponent" class="flex gap-1 align-middle pt-1">
-            <button
-                class="p-2 rounded-md bg-transparent hover:bg-[#363636] pb-1"
-                on:click={selectElement}
-            >
-                {#if import.meta.env.DEV}
-                    <img src={select} alt="select" width="15px" />
-                {:else}
-                    <img
-                        src={chrome.runtime.getURL(select)}
-                        alt="select"
-                        width="15px"
-                    />
-                {/if}
-            </button>
-            <input
-                class=" font-normal text-base text-white w-full min-w-[40px] h-8 bg-transparent outline-none border-b border-[#3e4446]"
-                type="text"
-                placeholder="select data or type here"
-                bind:value={field.value}
-            />
-        </div>
-        {#if index != 0}
-            <button
-                class="p-2 rounded-md bg-transparent hover:bg-[#363636]"
-                on:click={deleteField}
-            >
-                {#if import.meta.env.DEV}
-                    <img src={trash} alt="select" width="15px" />
-                {:else}
-                    <img
-                        src={chrome.runtime.getURL(trash)}
-                        alt="select"
-                        width="15px"
-                    />
-                {/if}
-            </button>
-        {/if}
-    {:else}
-        <div id="FieldComponent" class="flex gap-1 align-middle pt-1">
-            <p style="font-weight:700; font-size:16px">{key}</p>
-            <p>{value}</p>
-        </div>
-    {/if}
+	<!-- <p>{index}</p> -->
+	{#if isEditing}
+		<input
+			class="font-bold text-base text-white w-full min-w-[40px] h-8 bg-transparent outline-none border-b border-[#3e4446]"
+			type="text"
+			placeholder="enter key"
+			bind:value={field.key}
+		/>
+		<div id="FieldComponent" class="flex gap-1 align-middle pt-1">
+			<button
+				class="p-2 rounded-md bg-transparent hover:bg-[#363636] pb-1"
+				on:click={selectElement}
+			>
+				{#if import.meta.env.DEV}
+					<img src={select} alt="select" width="15px" />
+				{:else}
+					<img
+						src={chrome.runtime.getURL(select)}
+						alt="select"
+						width="15px"
+					/>
+				{/if}
+			</button>
+			<input
+				class=" font-normal text-base text-white w-full min-w-[40px] h-8 bg-transparent outline-none border-b border-[#3e4446]"
+				type="text"
+				placeholder="select data or type here"
+				bind:value={field.value}
+			/>
+		</div>
+		{#if index != 0}
+			<button
+				class="p-2 rounded-md bg-transparent hover:bg-[#363636]"
+				on:click={deleteField}
+			>
+				{#if import.meta.env.DEV}
+					<img src={trash} alt="select" width="15px" />
+				{:else}
+					<img
+						src={chrome.runtime.getURL(trash)}
+						alt="select"
+						width="15px"
+					/>
+				{/if}
+			</button>
+		{/if}
+	{:else}
+		<div id="FieldComponent" class="flex gap-1 align-middle pt-1">
+			<p style="font-weight:700; font-size:16px">{key}</p>
+			<p>{value}</p>
+		</div>
+	{/if}
 </div>
 
 <style>
--- src/components/Inspector.svelte
+++ src/components/Inspector.svelte
@@ -1,128 +1,128 @@
 <script lang="ts">
-    import DetailedSelector from "./DetailedSelector.svelte";
+	import DetailedSelector from "./DetailedSelector.svelte";
 
-    export let canvas: HTMLCanvasElement;
-    export let extensionId;
-    let selectedElement;
+	export let canvas: HTMLCanvasElement;
+	export let extensionId;
+	let selectedElement;
 
-    let hoveredElement;
+	let hoveredElement;
 
-    enum IdType {
-        ID,
-        CLASS,
-        INDEX,
-    }
+	enum IdType {
+		ID,
+		CLASS,
+		INDEX,
+	}
 
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (request.action === "closePopup") {
-            const popup = document.getElementById(`${extensionId}-iframe`);
-            popup.remove();
-            sendResponse({ success: true });
-        } else if (request.action === "inspect") {
-            inspect();
-            sendResponse({ success: true });
-        } else if (request.action === "bgValuesUpdate") {
-            console.log("in inspector: got bgValuesUpdate", request.paths);
-            let values = [];
-            request.paths.forEach((path, index) => {
-                values.push(getElementValueFromPath(path));
-            });
-            sendResponse({ success: true, values: values });
-        }
-    });
+	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		if (request.action === "closePopup") {
+			const popup = document.getElementById(`${extensionId}-iframe`);
+			popup.remove();
+			sendResponse({ success: true });
+		} else if (request.action === "inspect") {
+			inspect();
+			sendResponse({ success: true });
+		} else if (request.action === "bgValuesUpdate") {
+			console.log("in inspector: got bgValuesUpdate", request.paths);
+			let values = [];
+			request.paths.forEach((path, index) => {
+				values.push(getElementValueFromPath(path));
+			});
+			sendResponse({ success: true, values: values });
+		}
+	});
 
-    const inspect = async () => {
-        let click_count = 0;
+	const inspect = async () => {
+		let click_count = 0;
 
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        canvas.style.width = "100vw";
-        canvas.style.height = "100vh";
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		canvas.style.width = "100vw";
+		canvas.style.height = "100vh";
 
-        const ctx = canvas.getContext("2d");
+		const ctx = canvas.getContext("2d");
 
-        document.addEventListener("click", InspectElement, true);
-        document.addEventListener("mouseover", HoverElement, true);
-        window.addEventListener("mouseout", ClearCanvas, true);
+		document.addEventListener("click", InspectElement, true);
+		document.addEventListener("mouseover", HoverElement, true);
+		window.addEventListener("mouseout", ClearCanvas, true);
 
-        function InspectElement(event) {
-            click_count++;
-            event.preventDefault();
-            event.stopPropagation();
-            const element = event.target;
-            selectedElement = element;
+		function InspectElement(event) {
+			click_count++;
+			event.preventDefault();
+			event.stopPropagation();
+			const element = event.target;
+			selectedElement = element;
 
-            if (click_count == 1) {
-                console.log(selectedElement);
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                document.removeEventListener("click", InspectElement, true);
-                document.removeEventListener("mouseover", HoverElement, true);
-                window.removeEventListener("mouseout", ClearCanvas, true);
-            }
-        }
+			if (click_count == 1) {
+				console.log(selectedElement);
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				document.removeEventListener("click", InspectElement, true);
+				document.removeEventListener("mouseover", HoverElement, true);
+				window.removeEventListener("mouseout", ClearCanvas, true);
+			}
+		}
 
-        function HoverElement(event) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            hoveredElement = event.target;
-            ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
-            const hoveredDom = hoveredElement.getBoundingClientRect();
-            // ctx.fillRect(100, 100, 200, 200);
-            ctx.fillRect(
-                hoveredDom.left,
-                hoveredDom.top,
-                hoveredDom.width,
-                hoveredDom.height,
-            );
-        }
+		function HoverElement(event) {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			hoveredElement = event.target;
+			ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
+			const hoveredDom = hoveredElement.getBoundingClientRect();
+			// ctx.fillRect(100, 100, 200, 200);
+			ctx.fillRect(
+				hoveredDom.left,
+				hoveredDom.top,
+				hoveredDom.width,
+				hoveredDom.height
+			);
+		}
 
-        function ClearCanvas() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-        }
-    };
+		function ClearCanvas() {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		}
+	};
 
-    const getElementValueFromPath = (path) => {
-        const getElementFromCurrentPath = (currentPath, currentElement) => {
-            switch (currentPath.type) {
-                case IdType.ID:
-                    return currentElement.querySelector(
-                        "#" + currentPath.value,
-                    );
-                case IdType.CLASS:
-                    return currentElement.querySelectorAll(
-                        "." + currentPath.value,
-                    )[currentPath.index];
-                case IdType.INDEX:
-                    return currentElement.children[currentPath.index];
-            }
-        };
+	const getElementValueFromPath = (path) => {
+		const getElementFromCurrentPath = (currentPath, currentElement) => {
+			switch (currentPath.type) {
+				case IdType.ID:
+					return currentElement.querySelector(
+						"#" + currentPath.value
+					);
+				case IdType.CLASS:
+					return currentElement.querySelectorAll(
+						"." + currentPath.value
+					)[currentPath.index];
+				case IdType.INDEX:
+					return currentElement.children[currentPath.index];
+			}
+		};
 
-        let element;
-        console.log("in detail: getting element from: ", path);
-        element = getElementFromCurrentPath(path[0], document.body);
-        console.log("in detail: first path: ", element);
+		let element;
+		console.log("in detail: getting element from: ", path);
+		element = getElementFromCurrentPath(path[0], document.body);
+		console.log("in detail: first path: ", element);
 
-        for (let i = 1; i < path.length; i++) {
-            element = getElementFromCurrentPath(path[i], element);
-        }
+		for (let i = 1; i < path.length; i++) {
+			element = getElementFromCurrentPath(path[i], element);
+		}
 
-        return determineElementValue(element);
-    };
+		return determineElementValue(element);
+	};
 
-    const determineElementValue = (element) => {
-        if (element.nodeName == "IMG") {
-            return element.src;
-        } else {
-            return element.innerText;
-        }
-    };
+	const determineElementValue = (element) => {
+		if (element.nodeName == "IMG") {
+			return element.src;
+		} else {
+			return element.innerText;
+		}
+	};
 </script>
 
 {#if selectedElement}
-    <div>
-        <DetailedSelector
-            {extensionId}
-            bind:selectedElement
-            {getElementValueFromPath}
-        />
-    </div>
+	<div>
+		<DetailedSelector
+			{extensionId}
+			bind:selectedElement
+			{getElementValueFromPath}
+		/>
+	</div>
 {/if}
